30f81528707176bc643a5eda748536a1c837af27
Jonathan Sullivan <jj1of62003@gmail.com>
2018-06-14 20:36:03 -0500

    Added Model page for connecting to DB as well as connectors in app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flktst:test12@192.168.1.90/learningflask'
+db.init_app(app)
+
 
 @app.route("/")
 def index():
